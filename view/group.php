cbc0342a4358a17eedfd647f1f58920e6a73e17c
Dave <948009662@qq.com>
2017-09-12 23:17:55 +0800

    【Add】person首页

---
+++ view/group.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="https://cdn.bootcss.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
+	<style type="text/css">
+	</style>
+</head>
+<body>
+	
+</body>
+</html>
